0ad8c5e9a3ca18d783cc5afb7a7700442ced9d36
s-kenza <kenza.schuler@gmail.com>
2024-12-07 21:23:24 +0100

    fix: duplicate import

--- app/src/Controller/FrontController.php
+++ app/src/Controller/FrontController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\DBAL\Connection;
 
