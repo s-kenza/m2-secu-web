0badfb0242c96ab0f2ac012688b6a3b569320d23
s-kenza <kenza.schuler@gmail.com>
2024-12-07 13:38:10 +0100

    feat: add brute force

--- app/src/Controller/LoginController.php
+++ app/src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;  // Assurez-vous que le bon repository est utilisé
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,16 +10,38 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
+    private $userRepository;
+
+    // Injecter le repository UserRepository
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     #[Route('/login', name: 'app_login')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
+        $errorMessage = '';
+
+        // Vérifier si l'email existe dans la base de données
+        $user = $this->userRepository->findOneBy(['email' => $lastUsername]);
+
+        if ($error) {
+            // Si un utilisateur avec cet email existe, on vérifie le mot de passe
+            if ($user) {
+                // Si le mot de passe est incorrect
+                $errorMessage = 'Mot de passe incorrect';
+            } else {
+                // Si l'email n'existe pas dans la base de données
+                $errorMessage = 'Email incorrect';
+            }
+        }
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
-            'error' => $error,
+            'error' => $errorMessage,
         ]);
     }
 }
